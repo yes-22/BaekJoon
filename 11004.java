272fe142473d944dfb477620b6be50ab0e8edffd
yes-22 <155824945+yes-22@users.noreply.github.com>
2024-09-12 16:46:56 +0900

    Create 11004.java

---
+++ 11004.java
@@ -0,0 +1,21 @@
+import java.util*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        int K = scanner.nextInt();
+        int[] A = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            A[i] = scanner.nextInt();
+        }
+
+        Arrays.sort(A);
+
+        System.out.println(A[K - 1]);
+
+        scanner.close();
+    }
+}
